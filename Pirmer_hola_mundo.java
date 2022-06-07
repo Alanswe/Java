0703ce3644042aead8b9018e99eff8332d3847b9
Alanswe <alansweere@gmail.com>
2022-06-07 18:30:31 +0200

    Hola_mundo

---
+++ Pirmer_hola_mundo.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.pirmer_hola_mundo;
+
+/**
+ *
+ * @author alan
+ */
+public class Pirmer_hola_mundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("");
+    }
+}
