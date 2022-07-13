de6ff6c5c883f12dcee4af69d1c3f4fcb7664865
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-07-13 14:26:33 +0200

    15

---
+++ src/universo/Persona.java
@@ -0,0 +1,149 @@
+package universo;
+
+public class Persona {
+    private String nombre = "nombre no consta";
+    private String primerApellido = "apellido1 no consta";
+    private String segundoApellido = "apellido2 no consta";
+    private int DNISinLetra;
+    private char letraDNIMayusculas = ' ';
+    private int anioNacimiento;
+    private char genero = this.validaGenero(); // V o H o -;
+    
+    private int personaID; // se irá incrementando de uno en uno por cada nueva persona “instanciada”
+    
+    // Campos estáticos
+    private static long nextPersonaID;
+    // Se utilizará para saber cual es el próximo ID disponible.
+    
+    // Constructores
+    
+    Persona(String nombre, String primerApellido, String segundoApellido, char genero){
+        this(nombre,primerApellido,segundoApellido,genero,2022,0,' ');
+    }
+    
+    Persona(String nombre, String primerApellido, String segundoApellido, 
+            char genero, int anioNacimiento, int DNISinLetra, char letraDNIMayusculas){
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.genero = genero;
+        this.anioNacimiento = anioNacimiento;
+        this.DNISinLetra = DNISinLetra;
+        this.letraDNIMayusculas = letraDNIMayusculas;
+        this.personaID = (int)nextPersonaID++; // suma 1 para el siguiente ID
+    }
+    
+    
+    // Métodos
+    
+    String dniCompleto(){
+        String numeroDniCompleto = (this.DNISinLetra == 0 || this.letraDNIMayusculas == ' ') ? 
+                null: "" + this.DNISinLetra + this.letraDNIMayusculas;
+        return numeroDniCompleto;
+    };
+    
+    private char validaGenero(){
+        char generoFinalValidado = (this.genero == ' ')? '-':(this.genero == 'V') ?
+                'V':'H';
+        return generoFinalValidado;
+    }
+    
+    // valida genero ?
+    String sexo(){
+        String generoFinal = (this.genero == '-')? "No Definido":(this.genero == 'V') ?
+                "Varón":"Hembra";
+        return generoFinal;
+    }
+    
+    String nombreCompleto(){
+        return "" + this.nombre + " " + this.primerApellido + " " + this.segundoApellido;
+    }
+    
+    String show(){
+        return "DATOS PERSONA\n" + 
+                "=============\n" +
+                "Nombre: "+ this.nombre +"\n" +
+                "Apellidos: " + this.primerApellido + " " + this.segundoApellido + "\n" +
+                "DNI: "+ this.dniCompleto() + "\n" +
+                "Año de nacimiento: "+ this.anioNacimiento + "\n" +
+                "Edad: " + this.calculaEdad() + "\n" +
+                "Género: " + this.sexo();
+    }
+    
+    String showStatic(Persona p){
+        return p.show();
+    }
+    
+    int calculaEdad(){
+        return 2022 - this.anioNacimiento;
+    }
+    
+    // Métodos para test
+    
+    private static long getNextPersonaID() {
+        return nextPersonaID;
+    }
+    
+    // Gets para clase Planeta
+
+    public int getPersonaID() {
+        return personaID;
+    }
+    
+    
+    
+    // MAIN
+    public static void main(String[] args) {
+        Persona p1 = new Persona("Pepe","Dominguez","Fernandez",'V');
+        Persona p2 = new Persona("Antonia","Fernandez","Dominguez",'H',1996,04324567,'Y');
+        Persona p3 = new Persona("Juana","Fernandez","Dominguez",'-');
+
+        // Apartado 1
+        // SIN TERMIANR (NO FUNCIONA) 
+        System.out.println("Get ID");
+        System.out.println("=======");
+        System.out.println(p1.personaID);  
+        System.out.println(p2.personaID);
+        System.out.println(p2.getNextPersonaID());   
+        System.out.println("");
+        
+        // Apartado 2
+        
+        System.out.println("Prueba DniCompleto:");
+        System.out.println("==================");
+        System.out.println(p1.dniCompleto());
+        System.out.println(p2.dniCompleto());
+        System.out.println("");
+        
+        System.out.println("Prueba sexo:");
+        System.out.println("===========");
+
+        System.out.println(p1.sexo());
+        System.out.println(p2.sexo());
+        System.out.println(p3.sexo());
+        System.out.println("");
+        
+        System.out.println("Prueba nombreCompleto:");
+        System.out.println("=====================");
+
+        System.out.println(p1.nombreCompleto());
+        System.out.println("");
+        
+        System.out.println("Prueba show:");
+        System.out.println("============");
+
+        System.out.println(p2.show());
+        System.out.println("\n\n=== Estatico");
+        p2.showStatic(p3);
+        System.out.println("");
+        
+        
+        
+        }
+    
+
+
+    
+    
+    
+}
---
+++ src/universo/Planeta.java
@@ -0,0 +1,54 @@
+package universo;
+
+public class Planeta {
+    public static void main(String[] args) {
+        Persona p1 = new Persona("María","Pérez","Gómez",'H');
+        Persona p2 = new Persona("Pedro","Gutiérrez","Pérez",'V');
+        Persona p3 = new Persona("Antonio","Domínguez","Antúnez",'V');
+        Persona p4 = new Persona("Javier","Luque","Porras",'V');
+        Persona p5 = new Persona("Luisa","Mínguez","Mora",'H');
+        
+        // b
+        Persona p6 = new Persona("José","Mora","Larios",'V',2003,93849228,'M');
+        Persona p7 = new Persona("Rocío","Pérez","Pérez",'H',1967,94329532,'J');
+        Persona p8 = new Persona("Pilar","Laguna","López",'H',1999,83453624,'K');
+        Persona p9 = new Persona("David","Ravina","Díez",'V',1945,76532345,'A');
+        Persona p10 = new Persona("Juan","Lechuga","Montes",'V',2012,98432232,'Z');
+        
+        Persona[] personas = {p6,p7,p8,p9,p10};
+       
+        // Apartado 3
+       
+        // D
+        System.out.println("Listados de personas A:");
+        System.out.println("====================");
+        
+        for (Persona objetoPersona : personas){
+            System.out.println("ID : " + objetoPersona.getPersonaID() 
+                    + ", Nombre: " + objetoPersona.nombreCompleto() + ", DNI: " +
+                    objetoPersona.dniCompleto());
+        }        
+        System.out.println("");
+
+        
+        System.out.println("Listados de personas B:");
+        System.out.println("====================");
+        
+        for (Persona objetoPersona : personas){
+            if (objetoPersona.calculaEdad() >= 18) {
+                System.out.println("Nombre: " + objetoPersona.nombreCompleto() +
+                        ", Edad: " + objetoPersona.calculaEdad());
+            }       
+        }                
+        
+        System.out.println("");        
+        System.out.println("Verificación de lectura");
+        System.out.println("====================");
+        System.out.println(personas[1].show());
+        
+        
+        // 
+        
+        
+    }
+}
