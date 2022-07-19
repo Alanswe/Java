0e80f6ede6c77ff5277e81326235f38bef980a3a
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-07-19 13:52:13 +0200

    19

--- src/ModuloDos/InicializadorEstatico.java
+++ src/ModuloDos/InicializadorEstatico.java
@@ -6,8 +6,15 @@ public class InicializadorEstatico {
     
     static{System.out.println("Soy el primer inicializador estático");}
     
+    
+    
     public static void main(String[] args) {
+        {System.out.println("Soy un inicializador de la clase (no estatico)");} // tiene que estar en el main
+        
         System.out.println("Esto es en el main");
+        
+        {System.out.println("Soy otro inicializador de la clase");}
+
     }
     
     static{System.out.println("Soy el segundo inicializador estático");}
