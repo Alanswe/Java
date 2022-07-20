b96878aff50a480a2a9f74ca5a4152d26b2cfc29
Alanswe <92720019+Alanswe@users.noreply.github.com>
2022-07-20 12:18:16 +0200

    20

---
+++ src/ModuloDos/Enum.java
@@ -0,0 +1,36 @@
+package ModuloDos;
+
+public class Enum {
+    public enum WeekDay {
+	LU("Lunes", "Monday"), MA("Martes", "Tuesday"), 
+        MI("Miércoles", "Wednesday"), JU("Jueves", "Thursday"), 
+        VI("Viernes", "Friday"), SA("Sábado", "Saturday"), 
+        DO("Domingo", "Sunday"),;
+
+    private String longNameSP;
+    private String longNameEN;
+
+    private WeekDay(String longNameSP, String longNameEN){
+        this.longNameSP = longNameSP;
+        this.longNameEN = longNameEN;
+    }
+
+    public String getLongNameSP() {
+        return longNameSP;
+    }
+
+    public String getLongNameEN() {
+        return longNameEN;
+    }
+    
+    }
+
+    public static void main(String[] args) {
+        // Para llamar al método
+        WeekDay.MA.getLongNameSP();
+
+        System.out.println(WeekDay.MA.getLongNameSP()); 
+        System.out.println(WeekDay.MA.getLongNameEN());
+    }
+
+}
